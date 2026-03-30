b35f91afe4b3527f94bc2a118adfd23459dd6948
Prakhar Shukla <prakhar204shukla@gmail.com>
2026-03-30 09:42:42 +0530

    Update Script2_package_inspector.sh

--- Script2_package_inspector.sh
+++ Script2_package_inspector.sh
@@ -1,59 +1,56 @@
 #!/bin/bash
-# prakhar - 24BCE10117 - oss lab
-# checking if packages installed
+# prakhar shukla 24BCE10117
+# oss assignment - git audit
+# submitted late because wifi was down
 
-# my laptop uses ubuntu so dpkg works
-# FIXME: might not work on fedora but whatever
-
-pkg=$1
-
-# if no arg given, check git
-if [ "$pkg" == "" ]; then
-  pkg="git"
-fi
-
-echo "Checking package: $pkg"
-echo "========================"
-
-# check if installed - got this from stackoverflow
-# https://stackoverflow.com/questions/1298066
-dpkg -l "$pkg" 2>/dev/null | grep "^ii" > /dev/null
-if [ $? -eq 0 ]; then
-    echo "[OK] $pkg is installed"
-    
-    # get version number
-    ver=$(dpkg -l "$pkg" | grep "^ii" | awk '{print $3}')
-    echo "version: $ver"
-    
-    # show metadata
-    echo ""
-    echo "--- Package Info ---"
-    dpkg -s "$pkg" | grep "Package:" | head -1
-    dpkg -s "$pkg" | grep "Version:" | head -1
-    dpkg -s "$pkg" | grep "Maintainer:" || echo "no maintainer info"
-else
-    echo "[X] $pkg not found"
-    echo ""
-    echo "to install run:"
-    echo "sudo apt-get install $pkg"
-    echo ""
-    # echo "or download from website"  # commented out, not sure
-fi
-
-# license info - did this from memory might be wrong
+echo "========================================"
+echo "SYSTEM INFO - Git Open Source Audit"
+echo "========================================"
 echo ""
-echo "License:"
-if [ "$pkg" = "git" ]; then
-  echo "GPL v2"
-elif [ "$pkg" = "firefox" ]; then
-  echo "MPL something"
-elif [ "$pkg" = "vlc" ]; then
-  echo "LGPL i think"
-elif [ "$pkg" = "python3" ]; then
-  echo "PSF"
+
+# my info
+name="Prakhar Shukla"
+reg="24BCE10117"
+course="OSS"
+echo "Name: $name"
+echo "Reg: $reg"  
+echo "Course: $course"
+echo ""
+
+# debug: echo "starting system check"
+
+# system stuff - got these commands from google
+echo "Kernel: $(uname -r)"
+echo "User: $(whoami)"
+echo "Home Directory: $HOME"
+
+# distro check - lsb_release might not work on all systems
+# my friend said to use cat /etc/os-release but this works for me
+distro=$(lsb_release -d 2>/dev/null | cut -f2)
+if [ -z "$distro" ]; then
+    distro="Unknown Linux"
+    # TODO: fix this later
+fi
+echo "Operating System: $distro"
+
+# other system info
+echo "Uptime: $(uptime)"
+echo "Date/Time: $(date)"
+
+# check if git exists
+if which git > /dev/null 2>&1; then
+    git --version
 else
-  echo "probably GPL or MIT"
-  # check with: cat /usr/share/doc/$pkg/copyright 2>/dev/null | head -5
+    echo "git not installed"
 fi
 
-# debug: echo "done at $(date)" >> /tmp/script2.log
+echo ""
+echo "========================================"
+echo "License Information:"
+echo "Git is under GPL v2"
+echo "Linux kernel is also GPL v2"
+echo "Copyleft means you have to share source code"
+echo "========================================"
+
+# unused variable for future extension
+# semester="6th"
